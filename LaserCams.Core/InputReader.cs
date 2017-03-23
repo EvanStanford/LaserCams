2bfc183f542edc52697142476b2110c0a62e3ce6
estanford <evanstanford1@gmail.com>
2017-03-22 18:40:54 -0700

    Added csv reader and test

---
+++ LaserCams.Core/InputReader.cs
@@ -0,0 +1,32 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace LaserCams.Core
+{
+    public class InputReader
+    {
+        public Tuple<List<Point>, List<Point>> Read(string filePath)
+        {
+            List<InputRow> rows;
+            using (TextReader textReader = File.OpenText(filePath))
+            {
+                var csv = new CsvReader(textReader);
+                rows = csv.GetRecords<InputRow>().ToList();
+            }
+            return new Tuple<List<Point>, List<Point>>
+                (rows.Select(r => new Point { X = r.X1, Y = r.Y1 }).ToList(),
+                rows.Select(r => new Point { X = r.X2, Y = r.Y2 }).ToList());
+        }
+
+        private class InputRow
+        {
+            public double X1 { get; set; }
+            public double Y1 { get; set; }
+            public double X2 { get; set; }
+            public double Y2 { get; set; }
+        }
+    }
+}
---
+++ LaserCams.Tests/InputReaderTests.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+using LaserCams.Core;
+using System.Linq;
+
+namespace LaserCams.Tests
+{
+    public class InputReaderTests
+    {
+        [Fact]
+        public void ReadPositive()
+        {
+            var reader = new InputReader();
+            var result = reader.Read(@"C:\Dropbox\projects\LaserCams\LaserCams.Tests\Files\star cams.csv"); //required to use absolute paths because .net core is buggy https://github.com/xunit/xunit/issues/978
+
+            Assert.Equal(result.Item1.Count(), 35);
+            Assert.Equal(result.Item2.Count(), 35);
+            float maxExpected = 1000000;
+            float minExpected = -1000000;
+            Assert.True(result.Item1.All(p => p.X < maxExpected && p.X > minExpected && p.Y < maxExpected && p.Y > minExpected));
+        }
+    }
+}
